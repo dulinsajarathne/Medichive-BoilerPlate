67e08961a8fad56c6d59c9baa24bf0c8c69f1603
dulinsajarathne <dulinsajarathne2001@gmail.com>
2024-01-02 19:06:43 +0530

    second version

--- Medichive-BoilerPlate/src/App.jsx
+++ Medichive-BoilerPlate/src/App.jsx
@@ -1,35 +1,115 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+    HomeOutlined,
+    LogoutOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme } from 'antd';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Header, Sider, Content } = Layout;
+const App = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    return (
+        <Layout
+            style={{
+            minHeight:'100vh',
 
-export default App
+
+            borderRadius: borderRadiusLG,
+        }}>
+            <Sider trigger={null} collapsible collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)}
+                   style={{
+
+                       background:"#015A92",
+                       borderRadius: borderRadiusLG,
+                   }}
+            >
+
+                <Menu
+                    style={{
+
+                        background: "#015A92",
+                        borderRadius: borderRadiusLG,
+                    }}
+                    theme="light"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            icon: <HomeOutlined />,
+                            label: 'Home',
+
+                        },
+                        {
+                            key: '2',
+                            icon: <VideoCameraOutlined />,
+                            label: 'nav 2',
+
+                        },
+                        {
+                            key: '3',
+                            icon: <LogoutOutlined />,
+                            label: 'Logout',
+                        },
+                    ]}
+
+                />
+            </Sider>
+            <Layout>
+               <div className="header">
+                   <Header
+                           style={{
+                               display:"flex",
+                               padding: 0,
+                               backgroundColor: "#015A92",
+                           }}
+
+                   >
+
+
+                       <Button
+                           styles={{
+                               color:"white",
+
+                           }}
+                           type="text"
+                           icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                           onClick={() => setCollapsed(!collapsed)}
+                           style={{
+                               fontSize: '16px',
+                               width: 100,
+                               height: 64,
+                           }}
+
+                       />
+
+                       <div className="demo-logo">Medichive</div>
+                   </Header>
+               </div>
+                <Content
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: '90vh',
+                        minWidth:'100vw',
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
+export default App;
