0f2cb8e07421987ca685dead46512f88f81589b1
dulinsajarathne <dulinsajarathne2001@gmail.com>
2024-01-13 12:03:25 +0530

    eight version

---
+++ Medichive-BoilerPlate/src/side navigation/Logout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Logout = () => (
+    <div>
+        <h2>Logout</h2>
+        {/* Your Home component content goes here */}
+    </div>
+);
+export default Logout;
---
+++ Medichive-BoilerPlate/src/side navigation/Nav2.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Nav2 = () => (
+    <div>
+        <h2>Nav2</h2>
+        {/* Your Home component content goes here */}
+    </div>
+);
+export default Nav2;
